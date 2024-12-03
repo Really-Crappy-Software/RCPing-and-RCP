64154dd3cc1a67e65634efaf5e992b69db181444
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-12-03 20:15:29 +1100

    Add files via upload

--- rcping.c
+++ rcping.c
@@ -3,25 +3,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <linux/netlink.h>
+#include <linux/if_arp.h>
+#include <net/if.h>
 #include <asm/types.h>
 #include <libcap.h>
 #include <string.h>
-#define RCP 1560
+#define RCP 49152
 #define sizepkt 512
-int port;
-char IPv4[100] = "127.0.0.1";
-#define rcip(IPv4)
-#define rcpport(port)
-struct RCP_stuff {
+struct RCP_packet {
 uint16_t srcport;
 uint16_t extport;
 char* payload;
 };
 int main(int argc, char* argv[]) {
 int intextport = atoi(argv[1]);
-cap_init();
-    cap_t caps;
+    cap_t caps = cap_init();
         cap_value_t *capvl = 0;
             cap_set_flag(caps, CAP_NET_RAW, 1, capvl, CAP_SET);
 int sockfd = socket(AF_PACKET, SOCK_RAW, RCP);
@@ -32,25 +28,25 @@ if (sockfd < 0) {
     } else {
         printf("Socket Created successfully\n");
     }
-    struct sockaddr_in src_addr, dst_addr;
-    char* buffer;
-        dst_addr.sin_family = AF_INET;
-        dst_addr.sin_port = htons(intextport);
-        inet_pton(AF_INET, "8.8.8.8", &dst_addr.sin_addr);
-        src_addr.sin_family = AF_PACKET;
-        src_addr.sin_port = htons(1560);
-        inet_pton(AF_PACKET, IPv4, &src_addr.sin_addr);
+    struct sockaddr_in *dst_addr;
+    char buffer[sizepkt];
+        dst_addr->sin_family = AF_INET;
+        dst_addr->sin_port = htons(intextport);
+        inet_pton(AF_INET, "127.0.0.1", &dst_addr->sin_addr);
+        struct sockaddr_ll *src_addr;
+        src_addr->sll_family = AF_PACKET;
+        src_addr->sll_protocol = htons(1560);
+        src_addr->sll_ifindex = if_nametoindex("wlan0");
         struct sockaddr *addr = (struct sockaddr *)&dst_addr;
         struct sockaddr *interlcal = (struct sockaddr *)&src_addr;
-    rcpport(intextport);
-     struct RCP_stuff *packet;
-     packet->srcport = htons(1560);
+     struct RCP_packet *packet;
+     packet->srcport = htons(49152);
      packet->extport = htons(intextport);
      strcpy(packet->payload, "This packet was sent using RCPing under the Really Crappy Protocol");
         memcpy(buffer, &packet->srcport, sizeof(packet->srcport));
             memcpy(buffer + sizeof(packet->srcport), &packet->extport, sizeof(packet->extport));
                 memcpy(buffer + sizeof(packet->srcport) + sizeof(packet->extport), packet->payload, 8);
-                   struct RCP_stuff *rcp;
+                   struct RCP_packet *rcp;
         rcp->srcport = htons(1560);
         rcp->extport = htons(intextport);
         int miku = bind(sockfd, interlcal, sizeof(interlcal));
