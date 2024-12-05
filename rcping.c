58462ada09302e2ded298c0be1752d32e5b1f894
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-12-05 19:22:12 +1100

    Add files via upload

--- rcping.c
+++ rcping.c
@@ -3,18 +3,33 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <linux/if_arp.h>
+#include <net/if_arp.h>
 #include <net/if.h>
 #include <asm/types.h>
+#include <linux/if_ether.h>
 #include <libcap.h>
 #include <string.h>
-#define RCP 49152
-#define sizepkt 512
-struct RCP_packet {
+#define RCP             49152
+#define ARPHRD_ETHER 	1
+#define PACKET_RX       2
+#define sizepkt         512
+typedef unsigned short ushort_t;
+struct sockaddr_ll {
+    ushort_t sll_family;      /* address family (AF_LINK) */
+    ushort_t sll_protocol;    /* link-layer protocol (e.g., Ethernet) */
+    int32_t sll_ifindex;      /* interface index */
+    uint16_t sll_hatype;     /* ARP hardware type */
+    uint8_t  sll_pkttype;    /* packet type */
+    uint8_t  sll_halen;      /* hardware address length */
+    uint8_t  sll_addr[8];   /* hardware address (e.g., MAC address) */
+};
+
 uint16_t srcport;
 uint16_t extport;
-char* payload;
-};
+uint16_t srcport1;
+uint16_t extport2;
+char payload[256];
+
 int main(int argc, char* argv[]) {
 int intextport = atoi(argv[1]);
     cap_t caps = cap_init();
@@ -28,31 +43,79 @@ if (sockfd < 0) {
     } else {
         printf("Socket Created successfully\n");
     }
-    struct sockaddr_in *dst_addr;
+    printf("test1\n");
+    struct sockaddr_in *dst_addr = malloc((size_t)sizeof(struct sockaddr_in));
+    printf("1\n");
     char buffer[sizepkt];
+        printf("hi\n");
         dst_addr->sin_family = AF_INET;
+        printf("test2\n");
         dst_addr->sin_port = htons(intextport);
+        printf("test3\n");
         inet_pton(AF_INET, "127.0.0.1", &dst_addr->sin_addr);
-        struct sockaddr_ll *src_addr;
-        src_addr->sll_family = AF_PACKET;
-        src_addr->sll_protocol = htons(1560);
-        src_addr->sll_ifindex = if_nametoindex("wlan0");
+        printf("test4\n");
+        struct sockaddr_ll *src_addr = malloc((size_t)sizeof(struct sockaddr_ll));
+        int error1 = src_addr->sll_family = AF_PACKET;
+                if (error1 < 0) {
+            printf("socket family couldn't be set\n");
+            printf("%d\n", error1);
+            return -1;
+        }
+        int error2 = src_addr->sll_protocol = RCP;
+                if (error2 < 0) {
+            printf("The Internet protocol(IP) couldn't be set up\n");
+            printf("%d\n", error2);
+            return -1;
+        }
+        printf("test5\n");
+        int error3 = src_addr->sll_ifindex = if_nametoindex("wlan0");
+                if (error3 < 0) {
+            printf("local interface couldn't be set up\n");
+            printf("%d\n", error3);
+            return -1;
+        }
+        int error4 = src_addr->sll_pkttype = PACKET_RX;
+                if (error4 < 0) {
+            printf("package type couldn't be set\n");
+            printf("%d\n", error4);
+            return -1;
+        }
+        int error5 = src_addr->sll_halen = 6;
+                        if (error5 < 0) {
+            printf("package type couldn't be set\n");
+            printf("%d\n", error5);
+            return -1;
+        }
+        int error6 = src_addr->sll_hatype = ARPHRD_ETHER;
+                        if (error6 < 0) {
+            printf("package type couldn't be set\n");
+            printf("%d\n", error6);
+            return -1;
+        }
+        printf("test6\n");
         struct sockaddr *addr = (struct sockaddr *)&dst_addr;
+        printf("1\n");
         struct sockaddr *interlcal = (struct sockaddr *)&src_addr;
-     struct RCP_packet *packet;
-     packet->srcport = htons(49152);
-     packet->extport = htons(intextport);
-     strcpy(packet->payload, "This packet was sent using RCPing under the Really Crappy Protocol");
-        memcpy(buffer, &packet->srcport, sizeof(packet->srcport));
-            memcpy(buffer + sizeof(packet->srcport), &packet->extport, sizeof(packet->extport));
-                memcpy(buffer + sizeof(packet->srcport) + sizeof(packet->extport), packet->payload, 8);
-                   struct RCP_packet *rcp;
-        rcp->srcport = htons(1560);
-        rcp->extport = htons(intextport);
+     strcpy(payload, "This packet was sent using RCPing under the Really Crappy Protocol");
+     extport = htons(intextport);
+     srcport = htons(RCP);
+     char chrsrcport[16];
+     char chrextport[16];
+     sprintf(chrsrcport, "%d", srcport);
+     sprintf(chrextport, "%d", extport);
+        memcpy(buffer, chrsrcport, sizeof(chrsrcport));
+        printf("7\n");
+            memcpy(buffer + sizeof(chrsrcport), chrextport, sizeof(chrextport));
+            printf("8\n");
+                memcpy(buffer + sizeof(chrsrcport) + sizeof(chrextport), payload, 8);
+                printf("9\n");
+        srcport1 = htons(RCP);
+        extport2 = htons(intextport);
         int miku = bind(sockfd, interlcal, sizeof(interlcal));
+        printf("10\n");
         if (miku < 0) {
-            printf("Local interface couldn't be set up\n");
-            printf("%d", miku);
+            printf("client interface couldn't be set up\n");
+            printf("%d\n", miku);
             return -1;
         }
 
