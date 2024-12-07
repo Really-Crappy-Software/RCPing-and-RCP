bd854a9372afe7bca15d5fcc858c529237b8a27d
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-12-07 13:32:21 +1100

    Add files via upload

--- rcping.c
+++ rcping.c
@@ -15,7 +15,7 @@
 #define RCP            49152
 #define ARPHRD_ETHER 	1
 #define PACKET_RX       2
-#define sizepkt         512
+#define sizepkt         1024
 typedef unsigned short ushort_t;
 struct sockaddr_ll {
     ushort_t sll_family;      /* address family (AF_LINK) */
@@ -35,6 +35,8 @@ uint8_t  destip;
 char payload[256];
 
 int main(int argc, char* argv[]) {
+  clock_t start, end;
+  struct hostent *host_entry;
 int intextport = atoi(argv[1]);
     cap_t caps = cap_init();
         cap_value_t *capvl = 0;
@@ -55,14 +57,11 @@ if (sockfd < 0) {
     } else {
         printf("Socket 2 Created successfully\n");
     }
-    printf("test1\n");
     struct sockaddr_in *dst_addr = malloc((size_t)sizeof(struct sockaddr_in));
-    printf("1\n");
     char buffer[sizepkt];
-        printf("hi\n");
         dst_addr->sin_family = AF_INET; // IPv4 address family
         dst_addr->sin_port = htons(intextport); // port number
-        dst_addr->sin_addr.s_addr = inet_addr("8.8.8.8"); // IPv4 address
+        dst_addr->sin_addr.s_addr = inet_addr(argv[2]); // IPv4 address
     struct sockaddr_ll *src_addr = malloc((size_t)sizeof(struct sockaddr_ll));
         int error1 = src_addr->sll_family = AF_PACKET;
                 if (error1 < 0) {
@@ -101,52 +100,36 @@ if (sockfd < 0) {
             printf("%d\n", error6);
             return -1;
         }
-        printf("test6\n");
+      
         struct sockaddr *addr = (struct sockaddr *)dst_addr;
-        printf("1\n");
+        
         struct sockaddr *interlcal = (struct sockaddr *)src_addr;
-        printf("here\n");
-     printf("here\n");
         strcpy(payload, "This packet was sent using RCPing under the Really Crappy Protocol");
-        printf("here\n");
         version = 1.0;
-        printf("here\n");
-        meinip = inet_addr("220.233.193.89");
-        printf("here\n");
-        destip = inet_addr("8.8.8.8");
-        printf("here\n");
+        char ipaddr[16];
+        meinip = inet_addr("0.0.0.0");
+        destip = inet_addr(argv[2]);
         extport = htons(intextport);
-        printf("here\n");
         srcport = htons(RCP);
-        printf("here\n");
      char chrsrcport[16];
      char chrextport[16];
      char chrmeinip[8];
      char chrdestip[8];
      char chrversion[8];
-     printf("here\n");
-     sprintf(chrsrcport, "%d", srcport);
-     sprintf(chrextport, "%d", extport);
-     sprintf(chrversion, "%d", version);
-     sprintf(chrdestip, "%d", destip);
-     sprintf(chrmeinip, "%d", meinip);
-     printf("here\n");
-        memcpy(buffer, chrsrcport, sizeof(chrsrcport));
-        printf("7\n");
-            memcpy(buffer + sizeof(chrsrcport), chrextport, sizeof(chrextport));
-            printf("8\n");
-                memcpy(buffer + sizeof(chrsrcport) + sizeof(chrextport), payload, sizeof(payload));
-                printf("9\n");
-                    memcpy(buffer + sizeof(chrsrcport) + sizeof(chrextport) + sizeof(payload), chrversion, sizeof(chrversion));
-                    printf("10\n");
-                        memcpy(buffer + sizeof(chrsrcport) + sizeof(chrextport) + sizeof(payload) + sizeof(chrversion),  chrmeinip, sizeof(chrmeinip));
-                        printf("11\n");
-                            memcpy(buffer + sizeof(chrsrcport) + sizeof(chrextport) + sizeof(payload) + sizeof(chrversion) + sizeof(chrmeinip), chrdestip, sizeof(chrdestip));
-                            printf("12\n");
-
-
+     char chrpayload[256];
+     sprintf(chrsrcport, "client port:%d\n", srcport);
+     sprintf(chrextport, "port sent to:%d\n", extport);
+     sprintf(chrversion, "Version of RCP:%d\n", version);
+     sprintf(chrdestip, "Destination IP:%d\n", destip);
+     sprintf(chrmeinip, "Client IP:%d\n", meinip);
+     sprintf(chrpayload, "Payload:%s\n", payload );
+      strcat(buffer, chrsrcport);
+        strcat(buffer, chrextport);
+          strcat(buffer, chrversion);
+            strcat(buffer, chrdestip);
+              strcat(buffer, chrmeinip);
+                strcat(buffer, chrpayload);
         int miku = bind(sockfd, interlcal, sizeof(*src_addr));
-        printf("10\n");
         if (miku < 0) {
             printf("client interface couldn't be set up\n");
             printf("%d\n", miku);
@@ -159,12 +142,19 @@ if (sockfd < 0) {
         printf("%d\n", jumanji);
         return -1;
     } else {
+      printf("package sending:\n %s", buffer);
         while (main) {
+      sleep(1);
+      start = clock();
             ssize_t senresult = send(sockfd2, buffer, sizeof(buffer), 0);
+      end = clock();
             if (senresult < 0) {
-                printf("Package couldn't be sent");
+                printf("Package couldn't be sent\n");
                 return -1;
-            }
+            } else {
+        double thyme = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("package sent to %s, in %f seconds\n", argv[2], thyme);
+      }
         }
     }
 }
