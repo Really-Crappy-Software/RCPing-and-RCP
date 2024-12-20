3609bf8abf07050df4e712d869d1e9da52b088ad
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2024-12-20 02:49:10 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@ sudo cp libcap.h /usr/include
 sudo cp cap_names.h /usr/include
 sudo cp libcap.h /usr/local/include
 sudo cp cap_names.h /usr/local/include
-gcc rcping.c -o rcping
+gcc rcping.c -o rcping -lcap
 sudo cp rcping /usr/bin
